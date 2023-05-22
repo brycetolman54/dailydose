cb4db3a20889bf2add160d35e16ef3eae8b99e63
brycealantolman@gmail.com <brycealantolman@gmail.com>
2023-05-22 09:32:45 -0600

    made border thickness change with login/signup

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function switchForm(toWhat) {
     if(toWhat === "login") {
+        let element = document.querySelector('#login');
+        element.style.borderWidth = '2px';
+        let element2 = document.querySelector('#signup');
+        element2.style.borderWidth = '1px';
 
         let filler = document.querySelectorAll('.filler');
         for(let fill of filler) {
@@ -7,7 +11,11 @@ function switchForm(toWhat) {
         }
     }
     if(toWhat === "signup") {
-        
+        let element = document.querySelector('#signup');
+        element.style.borderWidth = '2px';
+        let element2 = document.querySelector('#login');
+        element2.style.borderWidth = '1px';
+
         let filler = document.querySelectorAll('.filler');
         for(let fill of filler) {
             fill.style.display = 'grid';
