4dcb1ff1ae0b2712389cb73e9847557c88468226
brycealantolman@gmail.com <brycealantolman@gmail.com>
2023-05-23 01:12:45 -0600

    created object to hold posts

--- feed.js
+++ feed.js
@@ -12,3 +12,24 @@ function backToLogin() {
     localStorage.removeItem('username');
     window.location.replace('index.html');
 };
+
+function addPost() {
+    // Let's get what we need from the form
+    const title = document.getElementById('postTitle').value;
+    const content = document.getElementById('postContent').value;
+
+    // Let's make an object to hold all this info
+    let obj = new Object;
+    obj.title = title;
+    obj.content = content;
+    obj.user = localStorage.getItem('username');
+    obj.time = new Date();
+    obj.comments = [];
+}
+
+function onLike() {
+
+}
+function offLike() {
+
+}
