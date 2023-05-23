db0a0ad1106efae485968f4dd590f3fc99e78bd7
brycealantolman@gmail.com <brycealantolman@gmail.com>
2023-05-23 02:19:04 -0600

    Made it so I can add posts to local storage

--- feed.js
+++ feed.js
@@ -1,3 +1,4 @@
+// This loads the username into the corner of the page
 window.addEventListener('DOMContentLoaded', () => {
     const user = localStorage.getItem('username');
     if(user) {
@@ -8,11 +9,31 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 });
 
+// This loads all the posts onto the page
+window.addEventListener('DOMContentLoaded', () => {
+    // Grab the data
+    const store = JSON.parse(localStorage.getItem('posts'));
+
+    // Go through each post and make it an html element
+    for(const post of store) {
+        rehydratePost(post);
+    }
+});
+
+// This is the actual function that we will be using to puff up the posts
+function rehydratePost(post) {
+    // First we find the feed field to add the post to
+    const feed = document.querySelector('#feedfield');
+    
+}
+
+// This takes us back to the login page if we click on the username
 function backToLogin() {
     localStorage.removeItem('username');
     window.location.replace('index.html');
 };
 
+// This allows us to add more posts to our local storage
 function addPost() {
     // Let's get what we need from the form
     const title = document.getElementById('postTitle').value;
@@ -24,7 +45,14 @@ function addPost() {
     obj.content = content;
     obj.user = localStorage.getItem('username');
     obj.time = new Date();
-    obj.comments = [];
+    // Maybe I'll put this in at a later date
+    // obj.comments = [];
+
+    // Pull the posts array out of storage to update it
+    const store = JSON.parse(localStorage.getItem('posts'));
+    store.unshift(obj);
+    localStorage.setItem('posts', JSON.stringify(store));
+    
 }
 
 function onLike() {
