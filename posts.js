518e2dee46b861578bdad84cc8d92b3b2122f7f4
brycealantolman@gmail.com <brycealantolman@gmail.com>
2023-05-26 02:37:42 -0600

    fixed table formatting of posts

--- posts.js
+++ posts.js
@@ -12,3 +12,18 @@ function backToLogin() {
     localStorage.removeItem('username');
     window.location.replace('index.html');
 };
+
+function closeOtherPosts(buttonId) {
+    // Get the posts element
+    const posts = document.getElementById('posts');
+    console.log(posts);
+
+    // Get its children
+    const children = posts.children;
+    console.log(children);
+
+    // Go through the children. If the buttonId doesn't match, change its checked attribute to false
+    for(const child in children) {
+        console.log(child[0].checked);
+    }
+}
