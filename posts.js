fd443940a27db11afbabd3ded2d12e4c69bf525a
brycealantolman@gmail.com <brycealantolman@gmail.com>
2023-05-29 09:49:33 -0600

    filling out table

--- posts.js
+++ posts.js
@@ -105,58 +105,80 @@ function addPost(post, allPosts) {
         // Add the id
         liEl.setAttribute('id', `post${post.myPlace}`);
         
-        // Add the checkbox
-        const check = document.createElement('input');
-            // Add the type
-            check.setAttribute('type', 'checkbox');
-            // Add the class
-            check.classList.add('reveal');
-            // Add the id
-            check.setAttribute('id', `reveal${post.myPlace}`);
-        // Put it in the liEl
-        liEl.appendChild(check);
-        
-        // Add the label
-        const label = document.createElement('label');
-            // Add the id
-            label.setAttribute('id',`label${post.myPlace}`);
-            // Add the for
-            label.setAttribute('for',`reveal${post.myPlace}`);
-            // Add the class
-            label.classList.add('label');
-            // Add event listener
-            label.addEventListener('click', `closeOtherPosts(${post.myPlace})`);
+            // Add the checkbox
+            const check = document.createElement('input');
+                // Add the type
+                check.setAttribute('type', 'checkbox');
+                // Add the class
+                check.classList.add('reveal');
+                // Add the id
+                check.setAttribute('id', `reveal${post.myPlace}`);
+            // Put it in the liEl
+            liEl.appendChild(check);
+            
+            // Add the label
+            const label = document.createElement('label');
+                // Add the id
+                label.setAttribute('id',`label${post.myPlace}`);
+                // Add the for
+                label.setAttribute('for',`reveal${post.myPlace}`);
+                // Add the class
+                label.classList.add('label');
+                // Add event listener
+                label.addEventListener('click', `closeOtherPosts(${post.myPlace})`);
 
-                // Add the head div
-                const headDiv = document.createElement('div');
-                    // Add the id
-                    headDiv.setAttribute('id',`head${post.myPlace}`);
-                    // Add the class
-                    headDiv.classList.add('head');
+                    // Add the head div
+                    const headDiv = document.createElement('div');
+                        // Add the id
+                        headDiv.setAttribute('id',`head${post.myPlace}`);
+                        // Add the class
+                        headDiv.classList.add('head');
 
-                        // Add the date
-                        const date = document.createElement('div');
-                            // Add the id
-                            date.setAttribute('id',`date${post.myPlace}`);
-                            // Add the class
-                            date.classList.add('date');
-                            // Set the content
-                            date.textContent = getDate(new Date(thisPost.time));
-                        // Put that in the head div
-                        headDiv.appendChild(date);
+                            // Add the date
+                            const date = document.createElement('div');
+                                // Add the id
+                                date.setAttribute('id',`date${post.myPlace}`);
+                                // Add the class
+                                date.classList.add('date');
+                                // Set the content
+                                date.textContent = getDate(new Date(thisPost.time));
+                            // Put that in the head div
+                            headDiv.appendChild(date);
 
-                        // Add the title
-                        const title = document.createElement('div');
-                            // Add the id
-                            title.setAttribute('id',`title${post.myPlace}`);
-                            // Add the class
-                            title.classList.add('title');
-                            // Set the content
-                            title.textContent = thisPost.title;
-                        // Put that in the head div
-                        headDiv.appendChild(title);
+                            // Add the title
+                            const title = document.createElement('div');
+                                // Add the id
+                                title.setAttribute('id',`title${post.myPlace}`);
+                                // Add the class
+                                title.classList.add('title');
+                                // Set the content
+                                title.textContent = thisPost.title;
+                            // Put that in the head div
+                            headDiv.appendChild(title);
 
+                            // Add the likes
+                            const like = document.createElement('div');
+                                // Add the id
+                                like.setAttribute('id',`like${post.myPlace}`);
+                                // Add the class
+                                like.classList.add('like');
+                                // Set the content
+                                like.textContent = thisPost.likes;
+                            // Put that in the head div
+                            headDiv.appendChild(like);
+                    
+                    // Add that head to the liEl
+                    liEl.appendChild(headDiv);
 
+                    // Add the content div
+                    const content = document.createElement('div');
+                        // Add the class
+                        content.classList.add('content');
+                        // Add the id
+                        content.setAttribute('id', `content${post.myPlace}`);
+                        // Set the content
+                        content.textContent = thisPost.content;
+                    liEl.appendChild(content);
 
     return liEl;
 }
