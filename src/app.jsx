0eee07f1efaf9c849dfccd5d726cd5838a9885d6
brycealantolman@gmail.com <brycealantolman@gmail.com>
2023-06-15 00:31:56 -0600

    created jsx files, set up header/footer for all

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import {BrowserRouter, NavLink, Route, Routes} from 
 'react-router-dom';
-// import './app.css';
+import './app.css';
 
 export default function App() {
     return (
-        <main>Hey there</main>
+        <BrowserRouter>
+            <div className='app'>        
+                <header>
+                    <h1>DailyDose</h1>
+                    <menu>
+                        <NavLink className="link" href="feed.html" to='feed'>Feed</NavLink>
+                        <NavLink className="link" href="chat.html" id="chatNav" to='chat'>Chat</NavLink>
+                        <NavLink className="link" href="posts.html" to='posts'>Posts</NavLink> 
+                    </menu>
+                </header>
+
+                <main>Content Here</main>
+
+                <footer>
+                    <p>Bryce Tolman</p>
+                    <a href="https://github.com/brycetolman54/startup.git">My GitHub</a>
+                </footer>
+            </div>
+        </BrowserRouter>
     )
 }
