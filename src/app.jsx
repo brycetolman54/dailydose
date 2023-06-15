bda9f3c44b04ad28f8d67c3074844c7dcaaee669
brycealantolman@gmail.com <brycealantolman@gmail.com>
2023-06-14 23:54:50 -0600

    moved files, started react app

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {BrowserRouter, NavLink, Route, Routes} from 
+'react-router-dom';
+// import './app.css';
+
+export default function App() {
+    return (
+        <main>Hey there</main>
+    )
+}
