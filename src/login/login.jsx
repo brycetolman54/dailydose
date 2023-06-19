0144bfdd0f24effecaf4799c2202acc2bfea02a0
brycealantolman@gmail.com <brycealantolman@gmail.com>
2023-06-19 09:56:05 -0600

    username check works

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -9,18 +9,33 @@ export function Login() {
     const [loginBorder, setLoginBorder] = React.useState('2px');
     const [suBorder, setSuBorder] = React.useState('0.5px');
 
+    const [username, setUsername] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+    const [error, setError] = React.useState('');
+
+    const [require1, setRequire1] = React.useState('\u2716');
+    const [require2, setRequire2] = React.useState('\u2716');
+
     React.useEffect(() => {
+
+        setUsername('');
+        setPassword('');
+
         if(show) {
             setLoginBorder('0.5px');
             setSuBorder('2px');
             setCheckShow('flex');
+            setError('');
         }
         else if(!show) {
             setLoginBorder('2px');
             setSuBorder('0.5px');
             setCheckShow('none');
+            setError('');
         }
     }, [show]);
+
 
     return (
         <main>
@@ -33,27 +48,27 @@ export function Login() {
                 <div id="loginForm">
                     <div id="loginbox">
                         <div id="error">
-                            {/* <!-- Inserted by code --> */}
+                            {error}
                         </div>
                         <div className="login"> 
                             <label htmlFor="text">Username:</label>
                             {/* oninput="checkUsername()" onkeydown="checkEnter(event, 'login')" */}
-                            <input type="text" id="loginText" name="userName" placeholder="Enter your username" required pattern="\w{5,15}"/>
+                            <input value={username} onChange={(e) => {setUsername(e.target.value); setRequire1(e.target.value.length >= 5 ? '\u2714' : '\u2716'); setRequire2((e.target.value.length <= 15 && e.target.value.length !== 0) ? '\u2714' : '\u2716');}} type="text" id="loginText" name="userName" placeholder="Enter your username" required pattern="\w{5,15}"/>
                         </div>
                         <div style={{display: checkShow}} className="filler">
                             <div id="require1" className="require">
-                                <div id="checkbox1" className="checkbox">&#x2716;</div>
+                                <div style={{color: require1 === '\u2714' ? 'green' : 'red'}} id="checkbox1" className="checkbox">{require1}</div>
                                 <div id="usernamecheck"><b>Minimum</b> 5 characters</div>
                             </div>
                             <div id="require1.5" className="require">
-                                <div id="checkbox2" className="checkbox">&#x2716;</div>
+                                <div style={{color: require2 === '\u2714' ? 'green' : 'red'}} id="checkbox2" className="checkbox">{require2}</div>
                                 <div id="usernamecheck"><b>Maximum</b> 15 characters</div>
                             </div>
                         </div>
                         <div className="login">
                             <label htmlFor="password">Password:</label>
                             {/* oninput="checkPassword()" onkeydown="checkEnter(event, 'login')" */}
-                            <input type="password" id="password" name="passWord" placeholder="Enter your password" required pattern="(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])[\S]{8,}" />
+                            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" id="password" name="passWord" placeholder="Enter your password" required pattern="(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])[\S]{8,}" />
                         </div>
                         <div style={{display: checkShow}} className="filler">
                             <div id="require2" className="require">
@@ -72,7 +87,7 @@ export function Login() {
                     </div>
                     <div className="login">
                     {/* onclick="submitForm('login')" */}
-                        <button type="submit" id="submit" >Submit</button>      
+                        <button disabled={!(username && password)} type="submit" id="submit" >Submit</button>      
                     </div>
                 </div>
             </div>
